3bc9fd62065283e0a0278327d53b2cbdfa734cfb
Adam Rehn <adam.rehn@my.jcu.edu.au>
2018-09-24 21:42:52 +1000

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name = 'maskutils',
-	version = '0.0.1',
+	version = '0.0.2',
 	description = 'Functionality for working with 2D image masks',
 	url = 'https://github.com/adamrehn/maskutils',
 	author = 'Adam Rehn',
